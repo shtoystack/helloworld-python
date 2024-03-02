aeb18af92e7d2365ddac387d3aec0744a66e6cc9
Juhi Dungrani <juhi@toystack.co>
2024-03-02 22:46:44 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+from flask import Flask 
+app = Flask(__name__) 
+  
+# routing the decorator function hello_name 
+@app.route('/')   
+def hello_name(): 
+   return 'Hello Toystack!' 
+  
+if __name__ == '__main__': 
+   app.run(debug = True) 
